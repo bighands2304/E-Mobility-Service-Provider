28d66cc3eb5b97b6e5f9c07367f7da25b4116c61
Ferro.90 <ferrettilorenzo2@gmail.com>
2023-01-29 15:13:26 +0100

    basic naviagation

--- Implementation/cpmsClient/cpms/src/App.js
+++ Implementation/cpmsClient/cpms/src/App.js
@@ -1,10 +1,15 @@
-import Landing from "./pages/Landing";
-import styled from "styled-components";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Error, Landing, Register, Dashboard } from "./pages";
 function App() {
   return (
-    <div>
-      <Landing />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="landing" element={<Landing />} />
+        <Route path="register" element={<Register />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- Implementation/cpmsClient/cpms/src/pages/Landing.js
+++ Implementation/cpmsClient/cpms/src/pages/Landing.js
@@ -1,17 +1,13 @@
-import logo3 from "../assets/images/logo3.svg";
-import styled from "styled-components";
+import { Logo } from "../components";
 import back1 from "../assets/images/back1.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
     <Wrapper>
       <nav>
-        <img
-          src={logo3}
-          alt="E-MALL logo"
-          className="e-mall"
-          style={{ width: "100px", height: "100px" }}
-        />
+        <Logo />
       </nav>
       <div className="container page">
         {/* info */}
@@ -20,48 +16,14 @@ const Landing = () => {
             E-MALL <span>CPMS</span>
           </h1>
           <p>Smart Charging Point Manager for E-MALL</p>
-          <button className="btn btn-hero">Login/Register</button>
+          <Link to="/register" className="btn btn-hero">
+            Login/Register
+          </Link>
         </div>
         <img src={back1} alt="job hunt" className="img main-img" />
       </div>
     </Wrapper>
   );
 };
-const Wrapper = styled.main`
-  nav {
-    width: var(--fluid-width);
-    max-width: var(--max-width);
-    margin: 0 auto;
-    height: var(--nav-height);
-    display: flex;
-    align-items: center;
-  }
-  .page {
-    min-height: calc(100vh - var(--nav-height));
-    display: grid;
-    align-items: center;
-    margin-top: -3rem;
-  }
-  h1 {
-    font-weight: 700;
-    span {
-      color: var(--primary-500);
-    }
-  }
-  p {
-    color: var(--grey-600);
-  }
-  .main-img {
-    display: none;
-  }
-  @media (min-width: 992px) {
-    .page {
-      grid-template-columns: 1fr 1fr;
-      column-gap: 3rem;
-    }
-    .main-img {
-      display: block;
-    }
-  }
-`;
+
 export default Landing;
