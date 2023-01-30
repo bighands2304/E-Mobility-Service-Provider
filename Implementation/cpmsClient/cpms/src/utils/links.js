7d40cb1e98e5dd4a659aa43a881b39d767af9f61
Ferro.90 <ferrettilorenzo2@gmail.com>
2023-01-30 13:33:09 +0100

    links ok

--- Implementation/cpmsClient/cpms/src/utils/links.js
+++ Implementation/cpmsClient/cpms/src/utils/links.js
@@ -4,10 +4,12 @@ import { FaWpforms } from "react-icons/fa";
 import { ImProfile } from "react-icons/im";
 
 const links = [
-  { id: 1, text: "stats", path: "/", icon: <IoBarChartSharp /> },
-  { id: 2, text: "all jobs", path: "all-jobs", icon: <MdQueryStats /> },
-  { id: 3, text: "add job", path: "add-job", icon: <FaWpforms /> },
-  { id: 4, text: "profile", path: "profile", icon: <ImProfile /> },
+  { id: 1, text: "stats", path: "/", icon: <ImProfile /> },
+  { id: 2, text: "all cps", path: "all-cps", icon: <ImProfile /> },
+  { id: 3, text: "add cp", path: "add-cp", icon: <ImProfile /> },
+  { id: 4, text: "map", path: "map", icon: <ImProfile /> },
+  { id: 5, text: "offers", path: "offers", icon: <ImProfile /> },
+  { id: 6, text: "profile", path: "profile", icon: <ImProfile /> },
 ];
 
 export default links;
