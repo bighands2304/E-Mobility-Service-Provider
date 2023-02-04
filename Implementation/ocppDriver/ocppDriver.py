718a81cdff873d39dde7d971c2768f46d6574a5b
Carlo Sgaravatti <carlo.sgaravatti@gmail.com>
2023-02-04 02:13:25 +0100

    update drivers

--- Implementation/ocppDriver/ocppDriver.py
+++ Implementation/ocppDriver/ocppDriver.py
@@ -15,6 +15,12 @@ MESSAGES_TO_SEND = ["StatusNotification", "MeterValues", "StartTransaction", "St
 
 CONTENT_TYPE = "application/json"
 
+MAPPING = {"ChangeAvailability/topic/ocpp/ChangeAvailability": "ChangeAvailability",
+           "CancelReservation/topic/ocpp/CancelReservation": "CancelReservation",
+           "ReserveNow/topic/ocpp/ReserveNow": "ReserveNow",
+           "SetChargingProfile/topic/ocpp/SetChargingProfile": "SetChargingProfile",
+           "ClearChargingProfile/topic/ocpp/ClearChargingProfile": "ClearChargingProfile"}
+
 
 class CpConnection:
 
@@ -25,16 +31,16 @@ class CpConnection:
         self.auth_key = auth_key
 
     def create_connection(self):
-        self.websock = websocket.WebSocketApp(f"wss://cpmsserver.up.railway.app/ocpp?token={self.auth_key}",
-                                              on_open=self.on_open,
-                                              on_message=self.on_message,
-                                              on_close=self.on_close,
-                                              on_error=self.on_error)
-        #self.websock = websocket.WebSocketApp(f"ws://localhost:8080/ocpp?token={self.auth_key}",
+        # self.websock = websocket.WebSocketApp(f"wss://cpmsserver.up.railway.app/ocpp?token={self.auth_key}",
         #                                      on_open=self.on_open,
         #                                      on_message=self.on_message,
         #                                      on_close=self.on_close,
         #                                      on_error=self.on_error)
+        self.websock = websocket.WebSocketApp(f"ws://localhost:8080/ocpp?token={self.auth_key}",
+                                              on_open=self.on_open,
+                                              on_message=self.on_message,
+                                              on_close=self.on_close,
+                                              on_error=self.on_error)
         self.websock.run_forever()
 
     def on_open(self, ws: websocket.WebSocketApp):
@@ -42,40 +48,46 @@ class CpConnection:
         sub_msg = stomper.subscribe("/topic/ocpp/BootNotification", self.cp_id)
         self.websock.send(sub_msg)
         self.websock.send(stomper.send("/app/ocpp/BootNotification",
-                                       json.dumps({"message": "Hi"}),
+                                       json.dumps({"cpId": self.cp_id}),
                                        content_type=CONTENT_TYPE))
 
     def on_message(self, ws, message):
         message_unpacked = stomper.unpack_frame(message)
+        body = json.loads(message_unpacked["body"])
+        destination: str = message_unpacked["headers"]["destination"]
         print(f"""
 ==========================================================================================
 current time: {datetime.datetime.now().isoformat()}
-message from cp: {self.cp_id} at destination {message_unpacked['headers']['destination']}
+message from cp: {self.cp_id} at destination {destination}
 the message is: 
 {message}
 The message parsed is 
 {message_unpacked}
-        """)
-        destination = message_unpacked["headers"]["destination"]
-        body = json.loads(message_unpacked["body"])
+                """)
         if destination == "/topic/ocpp/BootNotification":
             self.on_boot_notification_conf(body)
-        elif destination in ["/topic/ocpp/ChangeAvailability", "/topic/ocpp/CancelReservation",
-                             "/topic/ocpp/ReserveNow", "/topic/ocpp/SetChargingProfile",
-                             "/topic/ocpp/ClearChargingProfile"]:
+            return
+        prefix = len("/topic/" + self.cp_id)
+        destination = destination[prefix:]
+        print("corrected destination: " + destination)
+        if destination in ["ChangeAvailability/topic/ocpp/ChangeAvailability",
+                           "CancelReservation/topic/ocpp/CancelReservation",
+                           "ReserveNow/topic/ocpp/ReserveNow",
+                           "SetChargingProfile/topic/ocpp/SetChargingProfile",
+                           "ClearChargingProfile/topic/ocpp/ClearChargingProfile"]:
             request_id = message_unpacked["headers"]["requestId"]
-            self.on_message_to_accept("/app/" + destination[7:] + "Conf", request_id)
-        elif destination == "/topic/ocpp/RemoteStartTransaction":
+            self.on_message_to_accept("/app/ocpp/" + MAPPING[destination] + "Conf", request_id)
+        elif destination == "RemoteStartTransaction/topic/ocpp/RemoteStartTransaction":
             request_id = message_unpacked["headers"]["requestId"]
             self.on_remote_start_transaction(body, request_id)
-        elif destination == "/topic/ocpp/RemoteStopTransaction":
+        elif destination == "RemoteStopTransaction/topic/ocpp/RemoteStopTransaction":
             request_id = message_unpacked["headers"]["requestId"]
             self.on_remote_stop_transaction(body, request_id)
 
     def on_close(self, ws, a, b):
         print(f'''
 ==========================================================================================
-Connection with {self.cp_id} closed
+Connection with {self.cp_id} (session id = {self.session_id}) closed
 Trying to reconnect
 ==========================================================================================
         ''')
@@ -91,7 +103,7 @@ Trying to reconnect
     def on_boot_notification_conf(self, message):
         self.session_id = message["sessionId"]
         for topic in TOPICS:
-            sub_msg = stomper.subscribe(f"/topic/ocpp/{topic}", self.session_id)
+            sub_msg = stomper.subscribe(f"/topic/{self.cp_id + topic}/topic/ocpp/{topic}", self.cp_id + topic)
             self.websock.send(sub_msg)
 
     def on_message_to_accept(self, topic, request_id):
@@ -216,7 +228,7 @@ def open_connection():
 
 
 if __name__ == "__main__":
-    #t = threading.Thread(target=interactive_connections.loop)
-    #t.start()
+    # t = threading.Thread(target=interactive_connections.loop)
+    # t.start()
     app.run(host="0.0.0.0", port=5000)
-    #t.join()
+    # t.join()
