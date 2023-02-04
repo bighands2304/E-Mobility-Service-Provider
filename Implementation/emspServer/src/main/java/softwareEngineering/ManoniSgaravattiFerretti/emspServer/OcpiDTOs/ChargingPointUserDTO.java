5066080096748a14efe811bb7e0bb58846a1862d
bighands2304 <61188329+bighands2304@users.noreply.github.com>
2023-02-04 13:00:04 +0100

    + get cps return the tariffs in the body

---
+++ Implementation/emspServer/src/main/java/softwareEngineering/ManoniSgaravattiFerretti/emspServer/OcpiDTOs/ChargingPointUserDTO.java
@@ -0,0 +1,23 @@
+package softwareEngineering.ManoniSgaravattiFerretti.emspServer.OcpiDTOs;
+
+import lombok.Data;
+import softwareEngineering.ManoniSgaravattiFerretti.emspServer.ChargingPointDataModel.Model.ChargingPointOperator;
+import softwareEngineering.ManoniSgaravattiFerretti.emspServer.ChargingPointDataModel.Model.Socket;
+import softwareEngineering.ManoniSgaravattiFerretti.emspServer.ChargingPointDataModel.Model.Tariff;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class ChargingPointUserDTO {
+    private String id;
+    private String cpId;
+    private String name;
+    private String address;
+    private Double latitude;
+    private Double longitude;
+    private LocalDateTime lastUpdate;
+    private ChargingPointOperator cpo;
+    private List<Socket> sockets;
+    private List<Tariff> tariffs;
+}
