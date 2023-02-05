f21cd77b845b2fce1558d3af5b60f1ee1148fb3a
Ferro.90 <ferrettilorenzo2@gmail.com>
2023-02-05 16:17:53 +0100

    complete

--- Implementation/empsClient/MealsToGoProject/src/services/vehicles/mock/vehcile.context.js
+++ Implementation/empsClient/MealsToGoProject/src/services/vehicles/mock/vehcile.context.js
@@ -1,6 +1,7 @@
 import React, { useState, useContext, createContext, useEffect } from "react";
 import { AuthenticationContext } from "../../authentication/authentication.context";
 import { createOnButtonAlert } from "../../../components/utility/Alert";
+import { FavouritesContext } from "../../favourites/favourites.context";
 import {
   getVehicleRequest,
   vehicleTrasform,
@@ -15,11 +16,14 @@ export const VehicleContextProvider = ({ children }) => {
   const [vehicles, setVehicles] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  const {}
+
   // is just for try
   const { id, token } = useContext(AuthenticationContext);
 
   const RetrieveVehciles = (uid) => {
     console.log("retrive v for uid ==> " + uid);
+    console.log("with jwt VV" + JSON.stringify(token));
     setIsLoading(true);
     setVehicles([]);
 
